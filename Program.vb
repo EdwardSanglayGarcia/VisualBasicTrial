8e269cb4c8769ca414da149424479329664f493f
Edward Garcia <edward_12_1998@yahoo.com>
2022-07-31 21:27:09 +0800

    New changes

--- Program.vb
+++ Program.vb
@@ -147,3 +147,94 @@ Module SampleModule
         Console.WriteLine(NameOf(DoSomethingSecond))
     End Sub
 End Module
+
+
+'Microsoft Windows [Version 10.0.19043.1826]
+'(c) Microsoft Corporation. All rights reserved.
+
+'C:\Users\Edward>cd C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole
+
+'C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole>git remote add https://github.com/EdwardSanglayGarcia/VisualBasicTrial
+'fatal: Not a git repository (Or any of the parent directories): .git
+
+'C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole>git remote add origin https://github.com/EdwardSanglayGarcia/VisualBasicTrial
+'fatal: Not a git repository (Or any of the parent directories): .git
+
+'C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole>git remote add origin https://github.com/EdwardSanglayGarcia/VisualBasicTrial
+'fatal: Not a git repository (Or any of the parent directories): .git
+
+'C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole>git init
+'Initialized empty Git repository In C:/Users/Edward/Documents/Repositories/VisualBasicFinal/VisualBasicConsole/.git/
+
+'C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole>git remote add https://github.com/EdwardSanglayGarcia/VisualBasicTrial
+'usage: git remote add [<options>] <name> <url>
+
+'    -f, --fetch           fetch the remote branches
+'    --tags                import all tags And associated objects when fetching
+'                          Or do Not fetch any tag at all (--no-tags)
+'    -t, --track <branch>  branch(es) to track
+'    -m, --master <branch>
+'                          master branch
+'    --mirror[=(push|fetch)]
+'                          Set up remote As a mirror To push To Or fetch from
+
+
+'C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole>git remote add origin https://github.com/EdwardSanglayGarcia/VisualBasicTrial
+
+'C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole>git add .
+
+'C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole>git commit -m "Initial Codebase"
+'[master (root-commit) ba401d8] Initial Codebase
+' 27 files changed, 403 insertions(+)
+' create mode 100644 Program.vb
+' create mode 100644 VisualBasicConsole.vbproj
+' create mode 100644 bin/Debug/net6.0/VisualBasicConsole.deps.json
+' create mode 100644 bin/Debug/net6.0/VisualBasicConsole.dll
+' create mode 100644 bin/Debug/net6.0/VisualBasicConsole.exe
+' create mode 100644 bin/Debug/net6.0/VisualBasicConsole.pdb
+' create mode 100644 bin/Debug/net6.0/VisualBasicConsole.runtimeconfig.json
+' create mode 100644 obj/Debug/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.vb
+' create mode 100644 obj/Debug/net6.0/VisualBasicConsole.AssemblyInfo.vb
+' create mode 100644 obj/Debug/net6.0/VisualBasicConsole.AssemblyInfoInputs.cache
+' create mode 100644 obj/Debug/net6.0/VisualBasicConsole.GeneratedMSBuildEditorConfig.editorconfig
+' create mode 100644 obj/Debug/net6.0/VisualBasicConsole.assets.cache
+' create mode 100644 obj/Debug/net6.0/VisualBasicConsole.dll
+' create mode 100644 obj/Debug/net6.0/VisualBasicConsole.genruntimeconfig.cache
+' create mode 100644 obj/Debug/net6.0/VisualBasicConsole.pdb
+' create mode 100644 obj/Debug/net6.0/VisualBasicConsole.vbproj.AssemblyReference.cache
+' create mode 100644 obj/Debug/net6.0/VisualBasicConsole.vbproj.BuildWithSkipAnalyzers
+' create mode 100644 obj/Debug/net6.0/VisualBasicConsole.vbproj.CoreCompileInputs.cache
+' create mode 100644 obj/Debug/net6.0/VisualBasicConsole.vbproj.FileListAbsolute.txt
+' create mode 100644 obj/Debug/net6.0/apphost.exe
+' create mode 100644 obj/Debug/net6.0/ref/VisualBasicConsole.dll
+' create mode 100644 obj/Debug/net6.0/refint/VisualBasicConsole.dll
+' create mode 100644 obj/VisualBasicConsole.vbproj.nuget.dgspec.json
+' create mode 100644 obj/VisualBasicConsole.vbproj.nuget.g.props
+' create mode 100644 obj/VisualBasicConsole.vbproj.nuget.g.targets
+' create mode 100644 obj/project.assets.json
+' create mode 100644 obj/project.nuget.cache
+
+'C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole>git push
+'fatal: The current branch master has no upstream branch.
+'To push the current branch And set the remote as upstream, use
+
+'    git push --Set-upstream origin master
+
+
+'C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole>git push -u origin master
+'Enumerating objects :  32, done.
+'Counting objects :  100% (32/32), done.
+'Delta compression Using up To 4 threads
+'Compressing objects :  100% (25/25), done.
+'Writing objects :  100% (32/32), 88.33 KiB | 4.21 MiB/s, done.
+'Total 32(delta 2), reused 0 (delta 0), pack-reused 0
+'remote: Resolving deltas :  100% (2/2), done.
+'remote:
+'remote: Create a pull request For 'master' on GitHub by visiting:
+'remote: https : //github.com/EdwardSanglayGarcia/VisualBasicTrial/pull/New/master
+'remote:
+'To https//github.com/EdwardSanglayGarcia/VisualBasicTrial
+' * [new branch]      master -> master
+'branch 'master' set up to track 'origin/master'.
+
+'C:\Users\Edward\Documents\Repositories\VisualBasicFinal\VisualBasicConsole>
